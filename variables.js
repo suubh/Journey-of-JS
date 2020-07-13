fc619474674b286279f3d7dce3a0bbe382a1ac63
suubh <47265493+suubh@users.noreply.github.com>
2020-07-13 11:33:40 +0530

    Introduction of Variables

---
+++ variables.js
@@ -0,0 +1,22 @@
+//Sign-up for User
+var fullName="Shubham Singh";
+var emailId="subhdec99@gmail.com";
+var password="12345";
+var confirmPassword="12345";
+var isLoggedfromGoogle=false;
+const uniqueid="User123";
+
+
+console.log(`
+    ID := ${uniqueid}
+    Full Name is := ${fullName}
+    Email := ${emailId}
+    Password := ${password}
+    Confirm Password := ${confirmPassword}
+    User logged from Google := ${isLoggedfromGoogle}
+
+`);
+
+//or we can use console.log("Full Name is :" + fullName);
+//or console.log("Full Name is:" , fullName);
+//In that case we have to repeat console.log for each var.
