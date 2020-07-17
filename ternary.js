d0bd1ff3bc6085c4434c744744c69af7719f75d9
suubh <47265493+suubh@users.noreply.github.com>
2020-07-17 13:43:45 +0530

    Useful ternary operator.

---
+++ ternary.js
@@ -0,0 +1,11 @@
+//Show user sign out button if aunthenticated 
+//show user log in button if not auntheticated
+
+var aunthenticated=true;
+
+//ternanry operator
+
+aunthenticated ? console.log("Show Sign out button") : console.log("Show Login button");
+
+//The condition which is going to be evaluated ? True part : False part ;
+
