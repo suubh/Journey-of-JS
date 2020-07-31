f2a9ded5ece3efde1f6862674e8faf4a1dfbe124
suubh <subhdec99@gmail.com>
2020-07-31 17:51:33 +0530

    Slice and Splice

---
+++ sliceSpice.js
@@ -0,0 +1,14 @@
+var people=["Shub","Bill","Gates","Steve","Jobs","ANime","Billy"];
+
+console.log(people.slice(2,5));
+//Here 2 index is inclusive and 5th index is exclusive 
+//slice(start,end)
+//start is inclusive and end is exclusive 
+
+
+var user=["Shub","Bill","Gates","Steve","Jobs","ANime","Billy"];
+
+console.log(user.splice(2,5,"Hello","HI"));
+
+
+
