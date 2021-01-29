689c76e1b7bb1e5a14e308567f1de1fca6dffdba
suubh <subhdec99@gmail.com>
2021-01-29 14:39:02 +0530

    add

--- conditionals.js
+++ conditionals.js
@@ -1,9 +1,11 @@
-var temperature=23;
+var temperature=0;
 
 if(temperature>20){
     console.log("Its Hot outside");
 }
+else if(temperature==0){
+    console.log("Freeeeeezing !!");
+}
 else{
     console.log("It's Cold Oustide");
-
 }
