35ab744e3dac408aa19c50ed7e079734df9b1d65
suubh <subhdec99@gmail.com>
2021-02-01 16:35:48 +0530

    intro

---
+++ myintro.js
@@ -0,0 +1,9 @@
+const name = "Shubham Singh"
+var about = {
+    Passion : "(Machine Learning | Web Development)",
+    Skills : "(Experienced : C | Python | JavaScript) && (Familiar : Java | C++) && (Machine Learning)",
+    Frameworks : "TensorFlow | React | Express | Node ",
+    DBMS : "MySQL | MongoDb",
+    CS_Sub : "Data Structure | Algorithms | DBMS | OS | OOP | CN",
+    Extra : "Photography | Premiere Pro | Photoshop"
+}
