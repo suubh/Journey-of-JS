96eee2d5aed5fbf4535e02a2c203a33cbb46e000
suubh <subhdec99@gmail.com>
2021-02-03 17:59:55 +0530

    correction

--- myintro.js
+++ myintro.js
@@ -2,7 +2,7 @@ const name = "Shubham Singh"
 var about = {
     Passion : "(Machine Learning | Web Development)",
     Skills : "(Experienced : C | Python | JavaScript) && (Familiar : Java | C++) && (Machine Learning)",
-    Frameworks : "TensorFlow | React | Express | Node ",
+    Lib_Frameworks : "TensorFlow | React | Express | Node ",
     DBMS : "MySQL | MongoDb",
     CS_Sub : "Data Structure | Algorithms | DBMS | OS | OOP | CN",
     Extra : "Photography | Premiere Pro | Photoshop"
