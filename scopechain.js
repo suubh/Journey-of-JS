0dd92287afce49f4aa47a7253005f29e2496c147
suubh <subhdec99@gmail.com>
2021-02-27 13:06:53 +0530

    Corrections

--- scopechain.js
+++ scopechain.js
@@ -6,7 +6,7 @@ console.log("Line number 2 ",name);
      console.log("Line Number 6",name);
      sayNametwo();
      function sayNametwo(){
-         console.log("Line Number 9",name);//Now this name will look just one level up foe name and it will print "Shubham"
+         console.log("Line Number 9",name);//Now this name will look just one level up for name and it will print "Shubham"
          //If "Shubham " is not present than it will see one more level up and print "Shub"
          //Note:It will never look below or something ,it will always look above.
      }
